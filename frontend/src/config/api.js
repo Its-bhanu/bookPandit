d9cf3fb50fcbb8123d7e02f9bb584769c069c6d8
Its-bhanu <bhanu.22it498@rtu.ac.in>
2026-04-18 11:25:07 +0530

    api-change

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,6 +2,3 @@ const API_BASE = import.meta.env.VITE_API_BASE_URL || "https://book-pandit-mmed.
 const SOCKET_BASE = import.meta.env.VITE_SOCKET_BASE_URL || API_BASE;
 
 export { API_BASE, SOCKET_BASE };
-
-
-// https://book-pandit-mmed.vercel.app
